e8d0119bff46137e7209ddc0331ef445c3c0a668
theirix <theirix@gmail.com>
2017-07-20 23:39:36 +0300

    Upstream does not support Windows
    This reverts commit ef07f4cb34e1c16b2bf244b47cea059a3e25ff61.

--- conanfile.py
+++ conanfile.py
@@ -47,15 +47,10 @@ class AmqpcppConan(ConanFile):
         if self.options.shared:
             if self.settings.os == "Macos":
                 self.copy(pattern="*.dylib", dst="lib", keep_path=False)
-            elif self.settings.os == "Windows":
-                self.copy(pattern="*.dll", dst="lib", keep_path=False)
             else:
                 self.copy(pattern="*.so*", dst="lib", keep_path=False)
         else:
-            if self.settings.os == "Windows":
-                self.copy(pattern="*.lib", dst="lib", keep_path=False)
-            else:
-                self.copy(pattern="*.a", dst="lib", keep_path=False)
+            self.copy(pattern="*.a", dst="lib", keep_path=False)
 
     def package_info(self):
         self.cpp_info.libs = ['amqp-cpp']
