c49a5004a2f1bcf18b5f3762e389749c4d1faab4
theirix <theirix@gmail.com>
2017-12-03 20:50:43 +0300

    Upgraded to modern conan

--- conanfile.py
+++ conanfile.py
@@ -1,5 +1,4 @@
-from conans import ConanFile, CMake
-from conans.tools import download, untargz, check_sha1
+from conans import ConanFile, CMake, tools
 import os
 import shutil
 
@@ -9,7 +8,6 @@ class AmqpcppConan(ConanFile):
     url = "https://github.com/theirix/conan-amqpcpp"
     license = "https://github.com/CopernicaMarketingSoftware/AMQP-CPP/blob/master/LICENSE"
     description = "C++ library for asynchronous non-blocking communication with RabbitMQ"
-    FOLDER_NAME = 'AMQP-CPP-%s' % version
     settings = "os", "compiler", "build_type", "arch"
     options = {"shared": [True, False]}
     default_options = "shared=False"
@@ -17,38 +15,30 @@ class AmqpcppConan(ConanFile):
     generators = "cmake", "txt"
 
     def source(self):
-        tarball_name = "v%s.tar.gz" % self.version
-        download("https://github.com/CopernicaMarketingSoftware/AMQP-CPP/archive/%s" % tarball_name, tarball_name)
-        check_sha1(tarball_name, "76ba6f00d2ac96ef171da85bcfadd985e7d0b567")
-        untargz(tarball_name)
-        os.unlink(tarball_name)
-        shutil.move("%s/CMakeLists.txt" % self.FOLDER_NAME, "%s/CMakeListsOriginal.cmake" % self.FOLDER_NAME)
-        shutil.move("CMakeLists.txt", "%s/CMakeLists.txt" % self.FOLDER_NAME)
+        tools.get("https://github.com/CopernicaMarketingSoftware/AMQP-CPP/archive/v%s.tar.gz" % self.version)
+        os.rename("AMQP-CPP-%s" % (self.version), self.name)
 
     def configure(self):
         if self.settings.os == "Windows":
             raise Exception("Windows is not supported by upstream")
 
     def build(self):
-        cmake = CMake(self.settings)
+        shutil.move("%s/CMakeLists.txt" % self.name, "%s/CMakeListsOriginal.cmake" % self.name)
+        shutil.move("CMakeLists.txt", "%s/CMakeLists.txt" % self.name)
+        cmake = CMake(self)
 
-        # compose cmake options
-        extra_command_line = ''
-        if self.options.shared:
-            extra_command_line += " -DBUILD_SHARED=ON"
+        cmake.definitions['BUILD_SHARED'] = self.options.shared
         # avoid rpath
         if self.settings.os == "Macos":
-            extra_command_line += " -DCMAKE_SKIP_RPATH=ON"
+            cmake.definitions['CMAKE_SKIP_RPATH'] = True
 
-        cmd = 'cmake %s/%s %s %s' % (self.conanfile_directory, self.FOLDER_NAME, cmake.command_line, extra_command_line)
-        self.output.warn('Running CMake: ' + cmd)
-        self.run(cmd)
-        self.run("cmake --build . %s" % cmake.build_config)
+        cmake.configure(source_dir=self.name, build_dir="./")
+        cmake.build()
 
     def package(self):
-        self.copy("license*", src="%s" % (self.FOLDER_NAME), dst="licenses", ignore_case=True, keep_path=False)
-        self.copy("*.h", dst="include/amqpcpp", src="%s/include" % (self.FOLDER_NAME))
-        self.copy("amqpcpp.h", dst="include", src="%s" % (self.FOLDER_NAME))
+        self.copy("license*", src="%s" % (self.name), dst="licenses", ignore_case=True, keep_path=False)
+        self.copy("*.h", dst="include/amqpcpp", src="%s/include" % (self.name))
+        self.copy("amqpcpp.h", dst="include", src="%s" % (self.name))
         if self.options.shared:
             if self.settings.os == "Macos":
                 self.copy(pattern="*.dylib", dst="lib", keep_path=False)
@@ -58,6 +48,6 @@ class AmqpcppConan(ConanFile):
             self.copy(pattern="*.a", dst="lib", keep_path=False)
 
     def package_info(self):
-        self.cpp_info.libs = ['amqp-cpp']
+        self.cpp_info.libs = tools.collect_libs(self)
         if self.settings.os == "Linux":
             self.cpp_info.libs.extend(["pthread"])
--- test_package/conanfile.py
+++ test_package/conanfile.py
@@ -1,21 +1,19 @@
-from conans import ConanFile, CMake
+from conans import ConanFile, CMake, tools, RunEnvironment
 import os
 
 # This easily allows to copy the package in other user or channel
 channel = os.getenv("CONAN_CHANNEL", "stable")
 username = os.getenv("CONAN_USERNAME", "theirix")
 
-class DefaultNameConan(ConanFile):
-    name = "DefaultName"
-    version = "0.1"
+class TestPackageConan(ConanFile):
     settings = "os", "compiler", "build_type", "arch"
     requires = "amqpcpp/2.7.4@%s/%s" % (username, channel)
     generators = "cmake"
 
     def build(self):
-        cmake = CMake(self.settings)
-        self.run('cmake "%s" %s' % (self.conanfile_directory, cmake.command_line))
-        self.run("cmake --build . %s" % cmake.build_config)
+        cmake = CMake(self)
+        cmake.configure()
+        cmake.build()
 
     def imports(self):
         self.copy(pattern="*.dll", dst="bin", src="bin")
@@ -23,4 +21,10 @@ class DefaultNameConan(ConanFile):
         self.copy(pattern="*.dylib", dst="bin", src="lib")
 
     def test(self):
-        self.run("cd bin && .%stestapp" % os.sep)
+        with tools.environment_append(RunEnvironment(self).vars):
+            if self.settings.os == "Windows":
+                self.run(os.path.join("bin","testapp"))
+            elif self.settings.os == "Macos":
+                self.run("DYLD_LIBRARY_PATH=%s %s"%(os.environ.get('DYLD_LIBRARY_PATH', ''),os.path.join("bin","testapp")))
+            else:
+                self.run("LD_LIBRARY_PATH=%s %s"%(os.environ.get('LD_LIBRARY_PATH', ''),os.path.join("bin","testapp")))
