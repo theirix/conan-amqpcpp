10cb6184fbf03113571ee4b4ababcbb280a6d0c4
theirix <theirix@gmail.com>
2019-01-28 14:29:17 +0300

    Updated style

--- conanfile.py
+++ conanfile.py
@@ -1,18 +1,24 @@
-from conans import ConanFile, CMake, tools
 import os
 import shutil
+from conans import ConanFile, CMake, tools
 
 class AmqpcppConan(ConanFile):
     name = "amqpcpp"
     version = "3.0.0"
     url = "https://github.com/theirix/conan-amqpcpp"
+    author = "theirix@gmail.com"
+    homepage = "https://github.com/CopernicaMarketingSoftware/AMQP-CPP"
     license = "Apache-2.0"
     description = "C++ library for asynchronous non-blocking communication with RabbitMQ"
     settings = "os", "compiler", "build_type", "arch"
     options = {"shared": [True, False]}
-    default_options = "shared=False"
-    exports = "CMakeLists.txt"
+    default_options = {"shared": False}
     generators = "cmake"
+
+    exports = ["LICENSE.md"]
+    exports_sources = ["CMakeLists.txt"]
+
+    _source_subfolder = "source_subfolder"
 
     @property
     def majorver(self):
@@ -20,7 +26,11 @@ class AmqpcppConan(ConanFile):
 
     def source(self):
         tools.get("https://github.com/CopernicaMarketingSoftware/AMQP-CPP/archive/v%s.tar.gz" % self.version)
-        os.rename("AMQP-CPP-%s" % (self.version), self.name)
+        os.rename("AMQP-CPP-" + self.version, self._source_subfolder)
+        os.rename(os.path.join(self._source_subfolder, "CMakeLists.txt"),
+                  os.path.join(self._source_subfolder, "CMakeListsOriginal.txt"))
+        shutil.copy("CMakeLists.txt",
+                    os.path.join(self._source_subfolder, "CMakeLists.txt"))
 
     def configure(self):
         if self.settings.os == "Windows":
@@ -31,8 +41,6 @@ class AmqpcppConan(ConanFile):
             self.requires.add("OpenSSL/1.0.2n@conan/stable")
 
     def build(self):
-        shutil.move("%s/CMakeLists.txt" % self.name, "%s/CMakeListsOriginal.cmake" % self.name)
-        shutil.move("CMakeLists.txt", "%s/CMakeLists.txt" % self.name)
         cmake = CMake(self)
 
         if self.majorver == 2:
@@ -47,16 +55,17 @@ class AmqpcppConan(ConanFile):
         if self.settings.os == "Macos":
             cmake.definitions['CMAKE_SKIP_RPATH'] = True
 
-        cmake.configure(source_dir=self.name, build_dir="./")
+        cmake.configure(source_folder=self._source_subfolder)
+        # do not install because versions 2 and 3 differs a lot in layout
         cmake.build()
 
     def package(self):
-        self.copy("license*", src="%s" % (self.name), dst="licenses", ignore_case=True, keep_path=False)
+        self.copy("license*", src=self._source_subfolder, dst="licenses", ignore_case=True, keep_path=False)
         if self.majorver >= 3:
-            self.copy("*.h", dst="include", src="%s/include" % (self.name))
+            self.copy("*.h", dst="include", src=os.path.join(self._source_subfolder, "include"))
         else:
-            self.copy("*.h", dst="include/amqpcpp", src="%s/include" % (self.name))
-        self.copy("amqpcpp.h", dst="include", src="%s" % (self.name))
+            self.copy("*.h", dst="include/amqpcpp", src=os.path.join(self._source_subfolder, "include"))
+        self.copy("amqpcpp.h", dst="include", src=self._source_subfolder)
         if self.options.shared:
             if self.settings.os == "Macos":
                 self.copy(pattern="*.dylib", dst="lib", keep_path=False)
--- test_package/conanfile.py
+++ test_package/conanfile.py
@@ -10,16 +10,7 @@ class TestPackageConan(ConanFile):
         cmake.configure()
         cmake.build()
 
-    def imports(self):
-        self.copy(pattern="*.dll", dst="bin", src="bin")
-        self.copy(pattern="*.so", dst="bin", src="lib")
-        self.copy(pattern="*.dylib", dst="bin", src="lib")
-
     def test(self):
-        with tools.environment_append(RunEnvironment(self).vars):
-            if self.settings.os == "Windows":
-                self.run(os.path.join("bin","testapp"))
-            elif self.settings.os == "Macos":
-                self.run("DYLD_LIBRARY_PATH=%s %s"%(os.environ.get('DYLD_LIBRARY_PATH', ''),os.path.join("bin","testapp")))
-            else:
-                self.run("LD_LIBRARY_PATH=%s %s"%(os.environ.get('LD_LIBRARY_PATH', ''),os.path.join("bin","testapp")))
+        bin_path = os.path.join("bin", "test_package")
+        self.run(bin_path, run_environment=True)
+
